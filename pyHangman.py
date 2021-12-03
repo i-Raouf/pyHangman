1e122269d3cbf876b5ecddc88e1bdc74114fc61a
Raouf <iraouf86@gmail.com>
2021-12-03 22:31:45 +0100

    adding lifes feature

--- pyHangman.py
+++ pyHangman.py
@@ -12,11 +12,13 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
 
+    lives = 7
+
     # getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
         # what current word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -30,6 +32,7 @@ def hangman():
                 print('')
 
             else:
+                lives = lives - 1  # takes away a life if wrong
                 print('\nYour letter,', user_letter, 'is not in the word.')
 
         elif user_letter in used_letters:
@@ -39,6 +42,9 @@ def hangman():
             print('\nThat is not a valid letter.')
 
     # gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print('You died, sorry. The word was', word)
+    else:
         print('YAY! You guessed the word', word, '!!')
 
 if __name__ == '__main__':
