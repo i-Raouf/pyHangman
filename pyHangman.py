5e552b7176073265224f79ad8f7ace8d33b4ffa4
Raouf <iraouf86@gmail.com>
2021-12-03 22:05:46 +0100

    implement game logic

--- pyHangman.py
+++ pyHangman.py
@@ -6,5 +6,39 @@ def hangman(word):
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
 
+    # getting user input
+    while len(word_letters) > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
+        word_list = []
+        for letter in word:
+            if letter in used_letters:
+                word_list.append(letter)
+            else:
+                word_list.append("-")
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
+
+            else:
+                print('\nYour letter,', user_letter, 'is not in the word.')
+
+        elif user_letter in used_letters:
+            print('\nYou have already used that letter. Guess another letter.')
+
+        else:
+            print('\nThat is not a valid letter.')
+
+    # gets here when len(word_letters) == 0 OR when lives == 0
+        print('YAY! You guessed the word', word, '!!')
+
 if __name__ == '__main__':
     hangman("hello")
